8d077c43eb8576ae7e97280dc8e70f09f57695ae
Juan Pablo Olmos <jupaoljpol@gmail.com>
2012-03-18 05:56:01 -0600

    Adding Nuget packaging capabilities

--- Solutions/Build/Build.cmd
+++ Solutions/Build/Build.cmd
@@ -1,5 +1,5 @@
 SET NET_FRAMEWORK_DIR=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
 CALL "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat"
 
-msbuild NCastorBuild.proj /t:Build
+msbuild NCastorBuild.proj /t:RunTests
 pause
